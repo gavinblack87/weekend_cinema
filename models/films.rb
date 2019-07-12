42c198d47180b3f0cd0902361edb03dbaa025f24
gavinblack87 <gavin_black@hotmail.co.uk>
2019-07-12 15:37:43 +0100

    save methods

---
+++ models/films.rb
@@ -0,0 +1,30 @@
+require_relative('../db/sql_runner.rb')
+
+class Film
+
+attr_reader :id
+attr_accessor :title, :price
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @title = options['title']
+  @price = options['price'].to_i if options['id']
+end
+
+def save()
+    sql = "INSERT INTO films
+    (
+      title,
+      price
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@title, @price]
+    films = SqlRunner.run( sql, values ).first
+    @id = films['id'].to_i
+  end
+
+end
